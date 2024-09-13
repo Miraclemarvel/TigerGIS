45e813d278df4b1b8e56b95d5cf475585d1f9824
Miraclemarvel <117134149+Miraclemarvel@users.noreply.github.com>
2024-09-13 20:43:10 +0800

    Add files via upload

---
+++ log4cpp/Debug/NTEventLogCategories.h
@@ -0,0 +1,126 @@
+//
+//  Values are 32 bit values laid out as follows:
+//
+//   3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 1 1 1 1 1
+//   1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+//  +---+-+-+-----------------------+-------------------------------+
+//  |Sev|C|R|     Facility          |               Code            |
+//  +---+-+-+-----------------------+-------------------------------+
+//
+//  where
+//
+//      Sev - is the severity code
+//
+//          00 - Success
+//          01 - Informational
+//          10 - Warning
+//          11 - Error
+//
+//      C - is the Customer code flag
+//
+//      R - is a reserved bit
+//
+//      Facility - is the facility code
+//
+//      Code - is the facility's status code
+//
+//
+// Define the facility codes
+//
+
+
+//
+// Define the severity codes
+//
+
+
+//
+// MessageId: 0x00000001L (No symbolic name defined)
+//
+// MessageText:
+//
+// Fatal
+//
+
+
+//
+// MessageId: 0x00000002L (No symbolic name defined)
+//
+// MessageText:
+//
+// Alert
+//
+
+
+//
+// MessageId: 0x00000003L (No symbolic name defined)
+//
+// MessageText:
+//
+// Critical
+//
+
+
+//
+// MessageId: 0x00000004L (No symbolic name defined)
+//
+// MessageText:
+//
+// Error
+//
+
+
+//
+// MessageId: 0x00000005L (No symbolic name defined)
+//
+// MessageText:
+//
+// Warning
+//
+
+
+//
+// MessageId: 0x00000006L (No symbolic name defined)
+//
+// MessageText:
+//
+// Notice
+//
+
+
+//
+// MessageId: 0x00000007L (No symbolic name defined)
+//
+// MessageText:
+//
+// Info
+//
+
+
+//
+// MessageId: 0x00000008L (No symbolic name defined)
+//
+// MessageText:
+//
+// Debug
+//
+
+
+//
+// MessageId: 0x00000009L (No symbolic name defined)
+//
+// MessageText:
+//
+// NotSet
+//
+
+
+//
+// MessageId: 0x00001000L (No symbolic name defined)
+//
+// MessageText:
+//
+// %1
+//
+
+
